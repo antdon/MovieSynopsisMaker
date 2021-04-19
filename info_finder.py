3a365c3e02ddee55ea26ca935c088f45066c65fc
Anton <antondonohue2@protonmail.com>
2021-04-19 20:25:17 +1000

    refatored code adding documentation

--- info_finder.py
+++ info_finder.py
@@ -7,6 +7,13 @@ import sys
 #TODO - try catch for neterror
 
 class InfoFind():
+    """
+    Class containing functions relating to the creation of synopsis. 
+    Searchs wikipedia for title of movie and creates the string object of the first 
+    paragraph
+    Parameters:
+        title - name of movie
+    """
     def __init__(self, title: str) -> None:
         self.options = webdriver.FirefoxOptions()
         self.options.headless = True
@@ -14,25 +21,36 @@ class InfoFind():
         self.title = title
 
     def GetInfo(self) -> str:
+        """
+        Navigates from wikipedia home page to the page relating to the movie and
+        returns the first paragraph of that page
+        Return:
+            The first paragraph of the movie's wikipedia page
+        """
         ind: int = 0
+        #List containing the potential xcodes for the paragraph
         potential_synop_xpath = ['/html/body/div[3]/div[3]/div[5]/div[1]/p[2]','/html/body/div[3]/div[3]/div[5]/div[1]/p[4]']
         self.driver.get("https://www.wikipedia.org/")
         self.driver.find_element_by_name('search').send_keys(self.title)
         self.driver.find_element_by_xpath('/html/body/div[3]/form/fieldset/button').click()
+        #try catch to account for "did you mean" page
         try:
             self.driver.find_element_by_xpath('/html/body/div[3]/div[3]/div[4]/div[3]/ul/li[1]/div[1]/a').click()
         except NoSuchElementException:
             self.driver.find_element_by_xpath('//*[@id="mw-search-DYM-suggestion"]').click()
             self.driver.find_element_by_xpath('/html/body/div[3]/div[3]/div[4]/div[3]/ul/li[1]/div[1]/a').click()
         synop = self.driver.find_element_by_xpath('/html/body/div[3]/div[3]/div[5]/div[1]/p[2]')
-#       if synop.text == '':
-#           synop = self.driver.find_element_by_xpath('/html/body/div[3]/div[3]/div[5]/div[1]/p[4]')
+        #while loop to iterate through possible synop xpaths
         while synop.text == '' and ind != len(potential_synop_xpath) - 1:
             synop = self.driver.find_element_by_xpath(potential_synop_xpath[ind])
             ind += 1
+        #checks if synop is still empty and handels error
         if synop.text == '':
+            #if error is encountered change synop into string error message
             synop = "could not find synopsis" 
             print("could not find synopsis for " + self.title, file = sys.stderr) 
+        #since method returns a string, if synop is of type string synop itself
+        #can be returned 
         if  isinstance(synop, str):
             return synop
         else:
--- run.py
+++ run.py
@@ -6,6 +6,8 @@ import pdb
 if __name__ == "__main__":
     PATH = './movies'
     PATH = Path(PATH).resolve()
+    # iterate through directories calling SynopMaker on 
+    # each with dir title as title of movie
     for files in os.listdir(path=PATH):
         hemmingway = synop_maker.SynopMaker() 
         files_path = os.path.join(PATH, files)
